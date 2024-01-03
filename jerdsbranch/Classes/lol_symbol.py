e603dc4eecf86bc03ef3c09a5b2d17fb17c5a57b
angeloslvrs <aesoliveres@up.edu.ph>
2024-01-03 20:29:19 +0800

    del jerds branch

--- jerdsbranch/Classes/lol_symbol.py
+++
@@ -1,8 +0,0 @@
-"""
-    This file is used to define the symbol class of a symbol table for the compiler. 
-"""
-
-class SymbolEntity:
-    def __init__(self, symbolClassification, symbolValue):
-        self.symbolClassification = symbolClassification
-        self.symbolValue = symbolValue
