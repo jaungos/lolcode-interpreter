3d926046cda1ef89f22a2346797c2c0221a065d5
angeloslvrs <aesoliveres@up.edu.ph>
2024-01-06 20:08:18 +0800

    Edited main.py and contributors.txt

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 """
-    This file is the main file of the project. It is the one responsible for running the interpreter.
+    This file is the main file of the project. 
+    This is an interpreter for the LOLCode programming language created using Python.
 """
 
 from lolcode.interpreter import Interpreter
 
-# Initialize the lexical analyzer
-interpreter = Interpreter()
+def main():
+    # Initialize the lexical analyzer
+    interpreter = Interpreter()
 
-interpreter.read_file("testcases_natin/gimmeh_duplicate.lol")
-# interpreter.read_file("project-testcases/01_variables.lol")
+    interpreter.read_file("testcases_natin/gimmeh_duplicate.lol")
+    # interpreter.read_file("project-testcases/01_variables.lol")
 
-interpreter.run_lexer() # Run the lexical analyzer
+    interpreter.run_lexer() # Run the lexical analyzer
 
-interpreter.run_parser() # Run the syntax analyzer
+    interpreter.run_parser() # Run the syntax analyzer
 
-interpreter.run_interpreter() # Run the semantic analyzer
+    interpreter.run_interpreter() # Run the semantic analyzer
+
+# Run the program
+if __name__ == "__main__":
+    main()
