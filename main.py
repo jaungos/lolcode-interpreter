991e775372a28f2b2d6cc5b53ac666a685115ebc
angeloslvrs <aesoliveres@up.edu.ph>
2024-01-06 19:53:58 +0800

    Edited main.py and testcase and added contributor.txt

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 """
-    Developed by:   BONZA, Jhorge Anne B.
-                    SOLIVERES, Angelo E.
-                    UNGOS, Jerico Luis A.
+    This file is the main file of the project. It is the one responsible for running the interpreter.
 """
-
-"""
-    TODO:
-        - create a ReadMe file
-"""
-
 
 from lolcode.interpreter import Interpreter
 
 # Initialize the lexical analyzer
 interpreter = Interpreter()
 
-interpreter.read_file("testcases_natin/gimmeh.lol")
+interpreter.read_file("testcases_natin/gimmeh_duplicate.lol")
 # interpreter.read_file("project-testcases/01_variables.lol")
 
 interpreter.run_lexer() # Run the lexical analyzer
